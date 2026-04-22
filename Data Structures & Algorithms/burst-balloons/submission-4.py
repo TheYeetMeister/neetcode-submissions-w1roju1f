eb0af3e328d34ae42b98d871ff5150b70dd4bcba
TheYeetMeister <88285993+TheYeetMeister@users.noreply.github.com>
2026-04-22 13:14:47 -0700

    Add: burst-balloons - submission-4

---
+++ Data Structures & Algorithms/burst-balloons/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        numsPad = [1]
+        numsPad.extend(nums)
+        numsPad.append(1)
+
+        dp = {}
+        def dfs(left, right):
+            if left > right:
+               return 0 
+            elif left == right:
+                return numsPad[left - 1] * numsPad[left] * numsPad[left + 1]
+            elif (left, right) in dp:
+                return dp[(left, right)]
+
+            maxValue = 0
+            for value in range(left, right + 1):
+                leftSide = dfs(left, value - 1)
+                rightSide = dfs(value + 1, right)
+
+                popValue = numsPad[left - 1] * numsPad[value] * numsPad[right + 1]
+                maxValue = max(popValue + leftSide + rightSide, maxValue)
+
+            dp[(left, right)] = maxValue
+            return dp[(left, right)]
+
+        return dfs(1, len(numsPad) - 2)
+                    
