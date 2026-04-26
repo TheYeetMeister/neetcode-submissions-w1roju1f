d45911ff4521a0cf8da82071f24d81774801fa4f
TheYeetMeister <88285993+TheYeetMeister@users.noreply.github.com>
2026-04-26 16:06:21 -0700

    Add: prim - submission-0

---
+++ Data Structures & Algorithms/prim/submission-0.py
@@ -0,0 +1,48 @@
+class DSU:
+    def __init__(self, n):
+        self._parent = [i for i in range(n)]
+        self._rank = [0 for _ in range(n)]
+
+    def find(self, i):
+        if self._parent[i] == i:
+            return i
+
+        self._parent[i] = self.find(self._parent[i])
+        return self._parent[i]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX == rootY:
+            return False
+
+        if self._rank[rootX] > self._rank[rootY]:
+            self._parent[rootY] = rootX
+        elif self._rank[rootX] < self._rank[rootY]:
+            self._parent[rootX] = rootY
+        else:
+            self._rank[rootX] += 1
+            self._parent[rootY] = rootX
+        
+        return True
+
+class Solution:
+    def minimumSpanningTree(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        res = 0
+
+        minHeap = []
+
+        for u, v, w in edges:
+            heapq.heappush(minHeap, (w, u, v))
+
+        while minHeap:
+            weight, u, v = heapq.heappop(minHeap)
+
+            if dsu.union(u, v):
+                res += weight
+                n -= 1
+        
+        return res if n == 1 else -1
+        
