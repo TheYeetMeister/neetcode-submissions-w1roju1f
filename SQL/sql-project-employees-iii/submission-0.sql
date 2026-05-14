6ee38b61732b4720fb0b2a21ac1c7a2a93c08a04
TheYeetMeister <88285993+TheYeetMeister@users.noreply.github.com>
2026-05-14 09:46:16 -0700

    Add: sql-project-employees-iii - submission-0

---
+++ SQL/sql-project-employees-iii/submission-0.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+
+SELECT p.project_id, e.employee_id
+FROM project AS p
+JOIN employee AS e ON p.employee_id = e.employee_id
+WHERE (p.project_id, e.experience_years) IN (
+    SELECT p.project_id, MAX(e.experience_years)
+    FROM project AS p
+    INNER JOIN employee AS e ON p.employee_id = e.employee_id
+    GROUP BY p.project_id
+);
