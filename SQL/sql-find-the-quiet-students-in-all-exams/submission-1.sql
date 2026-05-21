b9b99d3743e1524ed906b6e04222eae691dd8efa
TheYeetMeister <88285993+TheYeetMeister@users.noreply.github.com>
2026-05-20 18:44:43 -0700

    Add: sql-find-the-quiet-students-in-all-exams - submission-1

---
+++ SQL/sql-find-the-quiet-students-in-all-exams/submission-1.sql
@@ -0,0 +1,21 @@
+-- Write your query below
+
+WITH loud_students AS (
+    SELECT DISTINCT e.student_id
+    FROM exam AS e
+    JOIN (
+        SELECT exam_id, MAX(score) highest, MIN(score) lowest
+        FROM exam
+        GROUP BY exam_id
+    ) AS t ON t.exam_id = e.exam_id AND (t.highest = e.score OR t.lowest = e.score)
+)
+
+SELECT student_id, student_name
+FROM student
+WHERE student_id IN (
+    SELECT DISTINCT student_id
+    FROM exam
+) AND student_id NOT IN (
+    select *
+    FROM loud_students
+);
