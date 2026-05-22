482e3b98efc9e221dff68a45f0f3c0de369919f1
TheYeetMeister <88285993+TheYeetMeister@users.noreply.github.com>
2026-05-22 11:36:32 -0700

    Add: roman-to-integer - submission-1

---
+++ Data Structures & Algorithms/roman-to-integer/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanDict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        largestVal = 0
+        res = 0
+
+        for i in reversed(s):
+            val = romanDict[i]
+
+            if val > largestVal:
+                largestVal = val
+
+            if largestVal > val:
+                res -= val
+            else:
+                res += val
+
+        return res
